202dd9e9bdd3a32b0658b08f8b0222c073e3e7ab
shafinmalik <57610548+shafinmalik@users.noreply.github.com>
2021-10-25 20:20:40 -0400

    Update

---
+++ tgsh/tgsh.go
@@ -0,0 +1,61 @@
+package tgsh
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+// Obtain device data as a slice of strings
+func deviceData() []string {
+	app := "xsetwacom"
+
+	arg0 := "list"
+	arg1 := "devices"
+
+	cmd := exec.Command(app, arg0, arg1)
+	stdout, err := cmd.Output()
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	// split devices into array of strings
+	loadout := strings.Split(string(stdout), "\n")
+	return loadout
+}
+
+// Convert slice of strings into individual strings
+func getName(ins string) string {
+	var name []string
+
+	temp := ins
+	seps := strings.Fields(temp)
+	k := len(seps)
+	for i := 0; i < k; i++ {
+		if seps[i] == "id:" {
+			name = append(seps[0:i])
+			fmt.Println(name)
+		}
+	}
+
+	devName := strings.Join(name, " ")
+	return devName
+}
+
+// Returns prepared slice of strings to ttd.go (input the returned values from the above functions)
+func nameData(loadout []string, size int) []string {
+	var names []string
+	for i := 0; i < size; i++ {
+		insert := getName(loadout[i])
+		names = append(names, insert)
+	}
+
+	return names
+}
+
+// Parse through a device and obtain button info
+
+// Parse through a device and obtain parameter info
+
+// Parse through the list of devices
---
+++ ttd/ttd.go
@@ -0,0 +1,33 @@
+package ttd
+
+// Device Struct
+type Device struct {
+	name   string
+	id     int
+	d_type string
+
+	// profiles
+	btnprofiles   []btnmaps
+	paramprofiles []params
+}
+
+type btnmaps struct {
+	btn []rune
+}
+
+type params struct {
+	MapToOutput     string
+	Area            string
+	Suppress        int
+	ToolDebugLevel  string
+	RawSample       int
+	Pressure        [4]int
+	Mode            string
+	Touch           string
+	Gesture         string
+	ZoomDistance    int
+	ScrollDistance  int
+	TapTime         int
+	CursorProximity int
+	Rotate          string
+}
